e0effbef9137e57baed77707f3bae0a52a8c5e8f
Holly Henske <hcover333@gmail.com>
2025-12-12 18:55:55 -0600

    Home page rendering with background

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../index.css'
+import App from '../App.jsx'
+
+
+export default function Home() {
+
+
+  return (
+    <div className="min-h-screen bg-gradient-main text-neutral9">
+      Home
+    </div>
+  );
+}
