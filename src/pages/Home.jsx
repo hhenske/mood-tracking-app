6f49a2368d94f7164c43c8f4363d76dcdfbd92f6
Holly Henske <hcover333@gmail.com>
2025-12-13 08:27:41 -0600

    logo in Header. avatar/dropdown icon are too big. Will need to separate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
-import '../index.css'
-import App from '../App.jsx'
+import '../index.css';
+import App from '../App.jsx';
+import Header from '../components/layout/Header.jsx';
 
 
 export default function Home() {
 
 
   return (
-    <div className="min-h-screen bg-gradient-main text-neutral9">
-      Home
+    <div className="min-h-screen bg-gradient-main text-neutral9 p-6">
+      <Header />
+
     </div>
   );
 }
